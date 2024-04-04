e2e134f70c4cb9272b4e51b19be9b2c3b2063b85
selimkucukkaraca <selimkkkaraca@hotmail.com>
2024-04-04 21:24:05 +0300

    Initial commit

---
+++ src/test/java/com/example/socialmediascanner/SocialMediaScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialmediascanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
